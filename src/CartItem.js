56b1f6ffeda365e57ec830c4ccf1c07d705b8efc
anaberia <ana.beria3@gmail.com>
2022-11-19 13:00:46 +0400

    added functionality on single cart remover button

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const CartItem = ({ id, img, title, price, amount }) => {
+  const { remove } = useGlobalContext();
   return (
     <article className="cart-item">
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
         <h4 className="item-price">${price}</h4>
-        <button className="remove-btn" onClick={() => console.log("remove")}>
+        <button className="remove-btn" onClick={() => remove(id)}>
           remove
         </button>
       </div>
